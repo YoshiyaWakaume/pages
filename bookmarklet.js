082fbc5eace5712a0590f12e16320245ea150839
YoshiyaWakaume <tukattyan.snow@gmail.com>
2024-03-15 14:53:11 +0900

    create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+  var script = document.createElement("script");
+  script.src = "https://YoshiyaWakaume.github.io/pages/disguiseGeoLocation.js";
+  document.head.appendChild(script);
+})();
