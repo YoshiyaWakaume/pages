c9ce0fb55b8962938393352f0a5fe248860a3927
YoshiyaWakaume <137358855+YoshiyaWakaume@users.noreply.github.com>
2024-03-15 15:07:09 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,1 @@
-javascript: (function () {
-  var script = document.createElement("script");
-  script.src = "https://YoshiyaWakaume.github.io/pages/disguiseGeoLocation.js";
-  document.head.appendChild(script);
-})();
+javascript:(function(){var%20script=document.createElement("script");script.src="https://YoshiyaWakaume.github.io/pages/disguiseGeoLocation.js";document.head.appendChild(script);})();
